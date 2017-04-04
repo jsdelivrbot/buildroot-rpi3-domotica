43a4e5479c39fe74a30a03c6f548e8557d0872b2
Jasper Nevens <jasper.nevens@linux.com>
2017-04-04 22:50:35 +0200

    libvsb: update to v0.1.1

--- package/libvsb/libvsb.mk
+++ package/libvsb/libvsb.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIBVSB_VERSION = v0.1.1
-LIBVSB_SOURCE = $(LIBVSB_VERSION).tar.gz
+LIBVSB_SOURCE = libvsb-$(LIBVSB_VERSION).tar.gz
 # Do not use the github helper here, the generated tarball is *NOT*
 # the same as the one uploaded by upstream for the release.
 LIBVSB_SITE = https://github.com/jnevens/libvsb/archive
@@ -16,5 +16,6 @@ LIBVSB_AUTORECONF = YES
 LIBVSB_CONF_OPTS = \
         --enable-vsbtools \
 	--enable-examples
+LIBVSB_INSTALL_STAGING = YES
 
 $(eval $(autotools-package))
