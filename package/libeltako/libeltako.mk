46b7e70f17e0ba3570ddd3a2f8c847f8aee820a8
Jasper Nevens <jasper.nevens@linux.com>
2017-04-04 22:48:39 +0200

    libeltako: update to v0.2.0

--- package/libeltako/libeltako.mk
+++ package/libeltako/libeltako.mk
@@ -4,23 +4,18 @@
 #
 ################################################################################
 
-LIBELTAKO_VERSION = v0.0.1
-LIBELTAKO_SOURCE = $(LIBELTAKO_VERSION).tar.gz
+LIBELTAKO_VERSION = v0.2.0
+LIBELTAKO_SOURCE = libeltako-$(LIBELTAKO_VERSION).tar.gz
 # Do not use the github helper here, the generated tarball is *NOT*
 # the same as the one uploaded by upstream for the release.
 # LIBELTAKO_SITE = https://github.com/jnevens/libeltako/archive/$(LIBELTAKO_VERSION).tar.gz
 LIBELTAKO_SITE = https://github.com/jnevens/libeltako/archive
-# LIBELTAKO_DEPENDENCIES = libnfc openssl
 LIBELTAKO_LICENSE = LGPLv2+ with exception
 LIBELTAKO_LICENSE_FILES = COPYING
 LIBELTAKO_AUTORECONF = YES
-#APACHE_CONF_OPTS = \
-#        --sysconfdir=/etc/apache2 \
-#        --with-apr=$(STAGING_DIR)/usr \
 
-#ifeq ($(BR2_STATIC_LIBS),y)
-# openssl needs zlib even if the libfreefare example itself doesn't
-#LIBELTAKO_CONF_ENV += LIBS='-lz'
-#endif
+define LIBELTAKO_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 755 package/libeltako/S45eltakod $(TARGET_DIR)/etc/init.d/S45eltakod
+endef
 
 $(eval $(autotools-package))
