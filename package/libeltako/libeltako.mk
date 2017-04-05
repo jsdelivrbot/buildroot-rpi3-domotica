16fc980aea33a19e4625cbe49f562f71143b8edd
Jasper Nevens <jasper.nevens@linux.com>
2017-04-05 21:59:38 +0200

    libeltako: fix build

--- package/libeltako/libeltako.mk
+++ package/libeltako/libeltako.mk
@@ -1,18 +1,11 @@
-################################################################################
-#
-# libfreefare
-#
-################################################################################
-
 LIBELTAKO_VERSION = v0.2.0
 LIBELTAKO_SOURCE = libeltako-$(LIBELTAKO_VERSION).tar.gz
-# Do not use the github helper here, the generated tarball is *NOT*
-# the same as the one uploaded by upstream for the release.
-# LIBELTAKO_SITE = https://github.com/jnevens/libeltako/archive/$(LIBELTAKO_VERSION).tar.gz
 LIBELTAKO_SITE = https://github.com/jnevens/libeltako/archive
 LIBELTAKO_LICENSE = LGPLv2+ with exception
 LIBELTAKO_LICENSE_FILES = COPYING
 LIBELTAKO_AUTORECONF = YES
+LIBELTAKO_DEPENDENCIES = libvsb
+LIBELTAKO_INSTALL_STAGING = YES
 
 define LIBELTAKO_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 755 package/libeltako/S45eltakod $(TARGET_DIR)/etc/init.d/S45eltakod
